09f3e172232e991771575f99bd4f02f928ed430b
joematheka693-del <joematheka@gmail.com>
2026-03-24 16:35:37 +0300

    New files

---
+++ assignment1.kt
@@ -0,0 +1,2 @@
+// create a file call it assignment1.kt => create an addition and subtraction programs inside of the said file
+// Research on conditional statements in kotlin
---
+++ lesson1a.kt
@@ -0,0 +1,23 @@
+// Type inference - let the compiler determine the data type of the variable
+fun main() {
+    // A variable is a container that stores data or values
+    // In kotlin we have two main types of initializers i.e: var and val
+    // val => immutable (unchangeable) but var => mutable (changeable)
+
+    val name = "Joe Matheka"  // String -> A series of characters inside of quotes
+
+    val number1 = 9  // Integer -> a whole number
+
+    val number2 = 40.6 // Double -> Number with a decimal
+
+    val letter = "J" // Char -> It is a single letter of the alphbet
+
+    val isRegistered = true // Boolean -> It evaluates either true or false
+
+
+    println(name)
+    println(number1)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+}
